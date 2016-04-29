e4bb9f292f3b24cdd9a014adbf77ebcf4d780d87
zengxianghong <zengxianghong@sialvan.com>
2016-04-29 23:09:45 +0800

    修改数据库连接

--- Public/db_config.php
+++ Public/db_config.php
@@ -1,10 +1,10 @@
 <?php
 return array(
-'DB_HOST'=>SAE_MYSQL_HOST_M,
-'DB_NAME'=>SAE_MYSQL_DB,
-'DB_USER'=>SAE_MYSQL_USER,
-'DB_PWD'=>SAE_MYSQL_PASS,
-'DB_PORT'=>SAE_MYSQL_PORT,
+'DB_HOST'=>'localhost',
+'DB_NAME'=>'invoice',
+'DB_USER'=>'zxh',
+'DB_PWD'=>'pdcxaje127',
+'DB_PORT'=>'3306',
 
   /*'DB_HOST'=>'localhost',
 'DB_NAME'=>'freecrm',
